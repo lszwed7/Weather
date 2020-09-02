a347d1d02de86c04093297f5965f622bec95b027
Lukas <lszwed7@gmail.com>
2020-09-02 08:41:23 +0200

    Updates

---
+++ precache-manifest.1bac3b8f0d3af1a35de6d73a918b1fad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa47c3d65f850b9abb0332150e65fdbf",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "af9069dfffe0f41d9e60",
+    "url": "/weather/static/css/main.0c41991e.chunk.css"
+  },
+  {
+    "revision": "06c3b8f4e0cff3b3c49a",
+    "url": "/weather/static/js/2.78747619.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather/static/js/2.78747619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9069dfffe0f41d9e60",
+    "url": "/weather/static/js/main.f00b6a41.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "43c7b7bc7b67af4a29925892c8076d1e",
+    "url": "/weather/static/media/400error.43c7b7bc.jpg"
+  },
+  {
+    "revision": "be3a05f0561ef1d57d6eea6f52f40c18",
+    "url": "/weather/static/media/wall2.be3a05f0.jpg"
+  }
+]);
